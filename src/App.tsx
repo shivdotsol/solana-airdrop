3e4e64717d6599f0410aaca344c5b8f23da290a3
shivdotsol <defi.shiv@gmail.com>
2025-08-30 17:52:02 +0530

    init

---
+++ src/App.tsx
@@ -0,0 +1,11 @@
+function App() {
+    return (
+        <>
+            <div className="w-screen h-screen p-10 bg-neutral-800 text-neutral-100">
+                Solana Airdrop
+            </div>
+        </>
+    );
+}
+
+export default App;
